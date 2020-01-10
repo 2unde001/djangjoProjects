93dd3896e95b83fbe4e0d768e0090440a9125fd4
Tunde Hammed <tunde0011@hotmail.com>
2020-01-10 16:24:58 +0000

    Removed unused methods and properties

--- form_project/formsApp/forms.py
+++ form_project/formsApp/forms.py
@@ -12,19 +12,16 @@ class UserForm(forms.Form):
     on our website
     """
     title = forms.CharField()
-    # name = forms.CharField(validators=[check_for_z])
     name = forms.CharField()
     email = forms.EmailField()
     very_email = forms.EmailField(label='Enter your email again:')
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False,
-    widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
 
     """
     Validate user email
     If user email do not match, clean the form, and display
     validation error
-    
+
     """
     def clean(self):
         all_clean_data = super().clean()
